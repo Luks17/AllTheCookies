2dc604d35c18d757efd6a41c288e0315fd7d9ecf
Luks17 <lucasthidavort@gmail.com>
2023-03-20 12:36:55 -0400

    Changed PageLayout.astro so Sidebar JS only loads on small screens

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -17,7 +17,7 @@ function Header() {
       <a href="/"><Logo /></a>
 
       {/* sidebar-toggle mobile */}
-      <button onClick={() => isSidebarOpen.set(!$isSidebarOpen)}>
+      <button className="md:hidden" onClick={() => isSidebarOpen.set(!$isSidebarOpen)}>
         <svg className="h8 w-8 text-skin-base" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
         </svg>
