9b0ca5a2de48b92635ac6ca676e593e2765d1fb0
Luks17 <lucasthidavort@gmail.com>
2023-03-31 10:07:32 -0400

    Updated all dependencies and changed mdx to markdoc

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import markdoc from "@astrojs/markdoc";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 
 // https://astro.build/config
 
@@ -14,13 +16,13 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-        vite: {
-                resolve: {
-                        alias: {
-                                "@": path.resolve(__dirname, "./src")
-                        }
-                }
-        },
-        integrations: [react(), tailwind(), mdx()],
-        site: "http://127.0.0.1:3000",
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src")
+      }
+    }
+  },
+  integrations: [react(), tailwind(), mdx(), markdoc()],
+  site: "http://127.0.0.1:3000"
 });
