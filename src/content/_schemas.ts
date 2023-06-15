02337881c1f2fc3035b152143bc95445cd84d59b
Luks17 <lucasthidavort@gmail.com>
2023-06-15 15:42:03 -0400

    I hope this works

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -1,17 +1,19 @@
 import { z } from "astro:content";
 
-export const postSchema = ({ image }) =>
-  z.object({
-    author: z.string(),
-    title: z.string(),
-    description: z.string(),
-    thumbnail: z.object({
-      img: image(),
-      alt: z.string(),
+export const postSchema = z.object({
+  author: z.string(),
+  title: z.string(),
+  description: z.string(),
+  thumbnail: z.object({
+    // TODO: change from endsWith(".png") to .webp
+    img: z.string().refine((img) => img.endsWith(".png"), {
+      message: "Thumbnail img must be webp",
     }),
-    publishDate: z.date(),
-    category: z.string(),
-    tags: z.array(z.string()),
-    draft: z.boolean(),
-    minutesRead: z.number(),
-  });
+    alt: z.string(),
+  }),
+  publishDate: z.date(),
+  category: z.string(),
+  tags: z.array(z.string()),
+  draft: z.boolean(),
+  minutesRead: z.number(),
+});
