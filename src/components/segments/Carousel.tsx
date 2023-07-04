7199edcefe8f3d10cd158fa1a2528ecae44ccaf3
Luks17 <lucasthidavort@gmail.com>
2023-07-03 22:57:29 -0400

    Post loading backend changes

--- src/components/segments/Carousel.tsx
+++ src/components/segments/Carousel.tsx
@@ -64,8 +64,9 @@ function Carousel({ sortedPosts }: Props) {
           className="flex w-min transition-transform ease-in-out duration-500"
           /* moves the ul to the correct post using only css */
           style={{
-            transform: `translateX(-${postIndex * (SLIDE_WIDTH + GAP_WIDTH)
-              }px)`,
+            transform: `translateX(-${
+              postIndex * (SLIDE_WIDTH + GAP_WIDTH)
+            }px)`,
             columnGap: `${GAP_WIDTH}px`,
           }}
         >
