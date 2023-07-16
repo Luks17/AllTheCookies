00ac435c543cd67b739d071735655a677ee4bf8a
Luks17 <lucasthidavort@gmail.com>
2023-07-16 19:06:19 -0400

    Removed Image tag and moved assets from /public to src/assets

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -2,15 +2,6 @@ import { defineMarkdocConfig, component } from "@astrojs/markdoc/config";
 
 export default defineMarkdocConfig({
   tags: {
-    image: {
-      render: component("./src/components/tags/Image.astro"),
-      attributes: {
-        src: { type: String },
-        alt: { type: String },
-        width: { type: Number },
-        height: { type: Number },
-      },
-    },
     aside: {
       render: component("./src/components/tags/Aside.astro"),
       attributes: {
