a389f3245c20a3ec3b56c31b321616febc29d06d
Luks17 <lucasthidavort@gmail.com>
2023-08-10 19:54:59 -0400

    Added more validations and written first post

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -1,19 +1,25 @@
 import { SITE } from "@/config.mjs";
 import { AuthorPossibleSocials } from "@/types/Authors";
-import { validateTags } from "@/util/post-validation";
+import { validateCategory, validateTags } from "@/util/post-validation";
 import { z, ImageFunction } from "astro:content";
 
 export const postSchema = ({ image }: { image: ImageFunction }) =>
   z.object({
     authors: z.array(z.string()),
-    title: z.string(),
-    description: z.string(),
+    title: z.string().refine((title) => title.length < 40, {
+      message: "Title cannot be longer than 40 characters",
+    }),
+    description: z.string().refine((desc) => desc.length < 200, {
+      message: "Description cannot be longer than 200 characters",
+    }),
     thumbnail: z.object({
       img: image(),
       alt: z.string(),
     }),
     publishDate: z.date(),
-    category: z.string(),
+    category: z.string().refine((category) => validateCategory(category), {
+      message: "Category in post does not exist",
+    }),
     tags: z.array(z.string()).refine((tags) => validateTags(tags), {
       message: `Cannot have more than ${SITE.maxTagsPerPost} tags.\nYour post tags may not be valid`,
     }),
