64ca51823cf07af4fef4ec643c54ea8a76c1a970
Luks17 <lucasthidavort@gmail.com>
2023-09-05 21:53:31 -0400

    Fixed some jankness and minor change to PageDesc

--- src/components/segments/Carousel.tsx
+++ src/components/segments/Carousel.tsx
@@ -71,22 +71,30 @@ function Carousel({ sortedPosts, maxIndex = SITE.postsPerPage }: Props) {
           className="flex w-min transition-transform ease-in-out duration-500"
           /* moves the ul to the correct post using only css */
           style={{
-            transform: `translateX(-${
-              postIndex * (SLIDE_WIDTH + GAP_WIDTH)
-            }px)`,
+            transform: `translateX(-${postIndex * (SLIDE_WIDTH + GAP_WIDTH)
+              }px)`,
             columnGap: `${GAP_WIDTH}px`,
           }}
         >
-          {posts.map((post, i) => (
-            <li key={i} style={{ width: SLIDE_WIDTH }}>
-              <Card
-                post={post}
-                useSmallImg={true}
-                special={i === 0}
-                viewTransitionStyle={true}
-              />
-            </li>
-          ))}
+          {posts.map((post, i) => {
+            const hide =
+              isLg && (i < postIndex || i > postIndex + (SHOWED_POSTS_LG - 1));
+            return (
+              <li
+                key={i}
+                className={`transition-opacity ease-in-out duration-500 ${hide ? "opacity-0" : ""
+                  }`}
+                style={{ width: SLIDE_WIDTH }}
+              >
+                <Card
+                  post={post}
+                  useSmallImg={true}
+                  special={i === 0}
+                  viewTransitionStyle={true}
+                />
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
